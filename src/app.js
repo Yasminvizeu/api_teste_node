04367781758476c1eb02f54e684183d2e7391bf1
Yasmin <vizeugilyasmin@gmail.com>
2024-07-30 15:53:41 -0300

    endpoit busca e deleta  seleção por id

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ import express from 'express';
 
 const app = express();
 
+//indica para o express ler body com json
+app.use(express.json());
 //mock
 const selecoes = [
     {id:1, selecao:'brasil', grupo:'G'}, 
     {id:2, selecao:'suica', grupo:'G'}, 
     {id:3, selecao:'camaroes', grupo:'G'}
-]
+];
+
+function buscarSelecoesPorId(id){
+    return selecoes.filter(selecao => selecao.id == id);
+};
+
+function buscaIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id);
+};
 
 // criar rota raiz
 app.get('/', (req, res) => {
@@ -16,6 +26,17 @@ app.get('/', (req, res) => {
 app.get('/selecoes',(req, res)=> {
     res.status(200).send(selecoes);
 });
-
+app.get('/selecoes/:id',(req,res) => {
+    res.json(buscarSelecoesPorId(req.params.id))
+});
+app.post('/selecoes',(req,res) => {
+    selecoes.push(req.body);
+    res.status(201).send('selecao cadastrada com sucesso');
+});
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id);
+    selecoes.splice(index, 1);
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso`);
+});
 export default app;
 
