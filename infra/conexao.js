43bc3f92b71fae68942d4b7921f50147d86ef2b5
Yasmin <vizeugilyasmin@gmail.com>
2024-07-31 14:05:24 -0300

    conxao mysql

---
+++ infra/conexao.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql'
+
+const conexao = mysql.createConnection({
+    host:'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'db_api_teste_node'
+});
+
+export default conexao;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js';
 const PORT = 3000;
 
-// escutar a porta
-app.listen(PORT, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
-});
+//fazer conexao
+conexao.connect((erro) => {
+    if(erro){
+        console.log(erro);
+    } else {
+        console.log("conexao realizada com sucesso");
+        // escutar a porta
+        app.listen(PORT, () => {
+        console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+        });
+    }
+})
